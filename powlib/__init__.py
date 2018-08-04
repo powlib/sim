56a6b2b44c720688e4dffa38020eb3b2e97fdc63
Andrew Powell <andrewandrepowell2@gmail.com>
2018-08-04 18:07:08 -0500

    Gonna take a break for now.

--- powlib/__init__.py
+++ powlib/__init__.py
@@ -23,10 +23,45 @@ class Namespace(object):
         full  = "{}({})".format(self.__class__.__name__,indat)
         return full
 
+    def issubsetof(self, namespace):
+        '''
+        Determines whether or not this namespace is a subset of the specified namespace. 
+        '''
+        return all( hasattr(namespace, mem) and getattr(namespace, mem)==val for mem, val in vars(self).items() )
+
+    def isequalto(self, namespace):
+        '''
+        Determines whether or not this namespace is equal to the specified namespace.
+        '''
+        return self.issubsetof(namespace) and namespace.issubsetof(self)
+
+    def __eq__(self, namespace):
+        '''
+        Determines whether or not this namespace is equal to the specified namespace.
+        '''
+        return self.isequalto(namespace)
+
 class Transaction(Namespace):
     '''
     Used to quickly create a Transaction, which
     is used in a lot of the cocotb high-level operations.
     '''
     pass
+
+class Interface(Namespace):
+    '''
+    Represents a group of cocotb handles.
+    '''    
+    _cntrl = []
+
+    def transaction(self, **data):
+        '''
+        Generates a transaction whose members 
+        are only the data signals.
+        '''
+        trans = {}
+        for name, handle in vars(self).items():
+            if name is not self._cntrl:
+                trans[name] = getattr(data, name, None)
+        return Transaction(**trans)                
 

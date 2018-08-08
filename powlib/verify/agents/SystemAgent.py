a55291a825702226114206d73416a85eb5350cd5
Andrew Powell <andrewandrepowell2@gmail.com>
2018-08-07 22:12:49 -0500

    Rewrote a bunch of stuff related to the system agent (really just drivers.)

--- powlib/verify/agents/SystemAgent.py
+++ powlib/verify/agents/SystemAgent.py
@@ -12,22 +12,17 @@ class ClockDriver(Driver):
     The clock driver simply drives the system's clocks.
     '''
 
-    def __init__(self, interface, period_trans, phase_trans=None, name=""):
+    def __init__(self, interface, param_namespace, name=""):
         '''
         Constructs the clock driver.
-        interface    = The interface associated with the driver should contain the
-                       cocotb handles to all the clocks that need to be driven.
-        period_trans = A transaction whose member names should match that of the 
-                       specified interface. The value associated with each member should
-                       be a 2-tuple representing the period of the clock.
-        phase_trans  = A transaction whose member names should match that of the
-                       specified interface. The value associated with each member should
-                       be a 2-tuple representing the phase of the clock.
-        name         = String identifier needed for logging.                       
+        interface       = The interface associated with the driver should contain the
+                          cocotb handles to all the clocks that need to be driven.
+        param_namespace = A namespace with parameters associated with each clock 
+                          in interface.
+        name            = String identifier needed for logging.                       
         '''
-        self.__period_trans = period_trans
-        self.__phase_trans  = phase_trans
-        self.__log          = SimLog("cocotb.clocks.{}".format(name))
+        self.__param_namespace = param_namespace
+        self.__log             = SimLog("cocotb.clocks.{}".format(name))
         Driver.__init__(self, interface)        
 
     @property
@@ -50,24 +45,30 @@ class ClockDriver(Driver):
         '''
 
         for name, handle in vars(self._interface).items():
-            period = getattr(self.__period_trans, name)
-            phase  = getattr(self.__phase_trans, name, (0,"ns"))
+            params     = getattr(self.__param_namespace, name, None)
+            period     = getattr(params, "period", None)            
+            phase      = getattr(params, "phase", None)
+            param_dict = {}
+            if period is not None: param_dict["period"] = period
+            if period is not None: param_dict["phase"]  = phase
             self.__log.info("Starting clock {} with period {} and phase {}...".format(name, period, phase))
-            fork(start_clock(clock=handle, period=period, phase=phase))
+            fork(start_clock(clock=handle, **param_dict))
 
 class ResetDriver(Driver):
     '''
-
+    This driver simply drives the system's resets.
     '''
 
-    def __init__(self, interface, active_mode_trans=None, associated_clock_trans=None, \
-                                  wait_cycles_trans=None, wait_time_trans=None, name=""):
+    def __init__(self, interface, param_namespace, name=""):
         '''
+        Constructor.                        
+        interface       = The interface associated with the driver should contain the
+                          cocotb handles to all the resets that need to be driven.
+        param_namespace = A namespace with parameters associated with each clock 
+                          in interface.
+        name            = String identifier needed for logging.           
         '''
-        self.__active_mode_trans      = active_mode_trans
-        self.__associated_clock_trans = associated_clock_trans
-        self.__wait_cycles_trans      = wait_cycles_trans
-        self.__wait_time_trans        = wait_time_trans
+        self.__param_namespace        = param_namespace
         self.__event                  = Event()
         self.__log                    = Simlog("cocotb.resets.{}".format(name))
         Driver.__init__(self, interface)
@@ -88,8 +89,8 @@ class ResetDriver(Driver):
 
         # Define a simply function for extracting a parameter corresponding
         # to a reset.
-        def add_param(param_trans, param_dict, param_name, reset_name):
-            param = getattr(param_trans, reset_name, None)
+        def add_param(params, param_name, param_dict):
+            param = getattr(params, param_name, None)
             if param is not None: param_dict[param_name] = param
 
         # The running coroutines need to be collected.
@@ -98,11 +99,12 @@ class ResetDriver(Driver):
         # Collect the parameters and run each reset's respective 
         # start reset coroutine.
         for name, handle in vars(self._interface).items():        
-            params = {}
-            add_param(param_trans=self.__active_mode_trans,      param_dict=params, param_name="active_mode",      reset_name=name)
-            add_param(param_trans=self.__associated_clock_trans, param_dict=params, param_name="associated_clock", reset_name=name)
-            add_param(param_trans=self.__wait_cycles_trans,      param_dict=params, param_name="wait_cycles",      reset_name=name)
-            add_param(param_trans=self.__wait_time_trans,        param_dict=params, param_name="wait_time",        reset_name=name)
+            params     = getattr(self.__param_namespace, name, None)
+            param_dict = {}
+            add_param(params=params, param_name="active_mode",      param_dict=param_dict)
+            add_param(params=params, param_name="associated_clock", param_dict=param_dict)
+            add_param(params=params, param_name="wait_cycles",      param_dict=param_dict)
+            add_param(params=params, param_name="wait_time",        param_dict=param_dict)
             self.__log.info("Initiating reset {}...".format(name))
             cos.append(start_reset(**params))
 
